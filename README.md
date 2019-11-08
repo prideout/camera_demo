4c96b5f3049c298882dc45c00a31e12b8c47e35f
Philip Rideout <philiprideout@gmail.com>
2019-11-07 21:36:07 -0800

    Tentative C API for nanort.

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Easy to build on macOS. First make sure you have CMake and clang installed, then
 # TODO
 
 - map mode
-    - write raycaster against the Z=0 plane
+    - use ray_float to write raycaster against the Z=0 plane
     - pan and zoom
+    - implement the nanort C interface (nanort_c.h)
     - navigation and animation (frames)
     - bounds
     - proper home position
@@ -14,3 +15,16 @@ Easy to build on macOS. First make sure you have CMake and clang installed, then
 - orbit mode
 - "show frame"
 - multi-touch to zoom
+
+# Notes
+
+JuliaComputing/llvm-cbe
+
+Screen-space Camera Constaints
+
+BLINN, J. 1988. Where am I? What am I looking at? In IEEE Computer Graphics and Applications. Vol. 22.
+179–188.
+
+GLEICHER, M. AND WITKIN, A. 1992. Through-the-lens camera control. In Siggraph, E. E. Catmull, Ed.
+Vol. 26. 331–340. ISBN 0-201-51585-7. Held in Chicago, Illinois.
+
--- extras/nanort/nanort_c.h
+++ extras/nanort/nanort_c.h
@@ -1,13 +1,55 @@
+// Vanilla C bindings to nanort by Philip Rideout.
+// Maybe one day this could be "par_tracer.h" but for now the impl uses C++.
+
 #ifndef NANORT_C_H
 #define NANORT_C_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void foo();
+typedef struct {
+  float org[3];
+  float dir[3];
+  float min_t;
+  float max_t;
+} part_ray;
+
+typedef struct {
+  float u;
+  float v;
+  float t;
+  size_t triangle_index;
+} part_intersection;
+
+typedef struct {
+  const float* vertices;
+  size_t num_vertices;
+  const uint16_t* triangles;
+  size_t num_triangles;
+} part_mesh;
+
+typedef struct {
+  float cost_t_aabb;
+  uint32_t min_leaf_primitives;
+  uint32_t max_tree_depth;
+  uint32_t bin_size;
+  uint32_t shallow_depth;
+  bool cache_bbox;
+  bool cull_backfaces;
+} part_config;
+
+typedef struct part_context_s part_context;
+
+part_context* part_create_context(part_config config, part_mesh mesh);
+
+void part_destroy_context(part_context* ctx);
+
+bool part_trace(const part_context* ctx, part_ray ray,
+                part_intersection* isect);
 
 #ifdef __cplusplus
 }
@@ -18,11 +60,54 @@ void foo();
 // -----------------------------------------------------------------------------
 #ifdef NANO_RT_C_IMPLEMENTATION
 
-#include <vector>
+#include <nanort/nanort.h>
 
-void foo() {
-  //
+struct part_context_s {
+  nanort::BVHBuildOptions<float> options;
+  nanort::TriangleMesh<float> mesh;
+  nanort::TriangleSAHPred<float> surfaceAreaHeuristic;
+  nanort::BVHAccel<float> accel;
+};
+
+part_context* part_create_context(part_config config, part_mesh mesh) {
+  part_context* context = PAR_CALLOC(part_context, 1);
+  // TODO
+  return context;
 }
+
+void part_destroy_context(part_context* ctx) { PAR_FREE(ctx); }
+
+bool part_trace(const part_context* ctx, part_ray ray,
+                part_intersection* isect) {
+  // TODO
+  return true;
+}
+
+#if 0
+void foo() {
+  nanort::BVHBuildOptions<float> options;
+
+  nanort::TriangleMesh<float> triangleMesh(
+      &mesh.vertices.at(0), &mesh.faces.at(0), sizeof(float) * 3);
+
+  nanort::TriangleSAHPred<float> surfaceAreaHeuristic(
+      &mesh.vertices.at(0), &mesh.faces.at(0), sizeof(float) * 3);
+
+  nanort::BVHAccel<float> accel;
+
+  ret = accel.Build(mesh.faces.size() / 3, triangleMesh, trianglePred, options);
+
+  nanort::BVHAccel<float> accel;
+  nanort::TriangleIntersector<float> isecter(
+      triangleMesh.vertices_, triangleMesh.faces_, sizeof(float) * 3);
+
+  nanort::TriangleIntersection<float> isect;
+  bool hit = accel.Traverse(ray, isecter, &isect);
+  if (hit) {
+    occlusion += 1.0f;
+  }
+}
+#endif
 
 #endif  // NANO_RT_C_IMPLEMENTATION
 #endif  // NANO_RT_C_H
